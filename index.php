3b31b70456cb3427daa13ee0d2ab964fcc8cf7d7
Noah Scholfield <contact@noahscholfield.com>
2018-11-19 17:46:40 -0500

    Fix more stuff

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php session_start(); ?>
-<h1>Test</h1>
-<?php print_r($_SESSION); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      DEFINE("PAGE_TITLE", "Home"); // change "Home" to the title of the page
+      // Need to change this to "../partials/head.php" if inside a folder
+      require('partials/head.php');
+    ?>
+    <!-- Any extra stylesheets/css goes here -->
+  </head>
+  <body>
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">Test</h1>
+      </div>
+    </div>
+    <div class="container">
+      <!-- Need to change this to "../partials/navbar.php" if inside a folder -->
+      <?php include('partials/navbar.php'); ?>
+      <!-- This is where the main page content goes -->
+    </div>
+    <!-- Need to change this to "../partials/footer.php" if inside a folder -->
+    <?php include('partials/footer.php'); ?>
+    <!-- Any extra javascript goes down here -->
+  </body>
+</html>
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,27 +1,27 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="<?php path('/'); ?>"><span class="far fa-images"></span> Framed</a>
+  <a class="navbar-brand" href="/"><span class="far fa-images"></span> Framed</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link <?php if(PAGE_TITLE == 'Home') echo 'active'; ?>" href="<?php path('/'); ?>">Home</a>
+        <a class="nav-link <?php if(PAGE_TITLE == 'Home') echo 'active'; ?>" href="/">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php if(PAGE_TITLE == 'About') echo 'active'; ?>" href="<?php path('/about/'); ?>">About</a>
+        <a class="nav-link <?php if(PAGE_TITLE == 'About') echo 'active'; ?>" href="">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php if(PAGE_TITLE == 'Store') echo 'active'; ?>" href="<?php path('/store/'); ?>">Store</a>
+        <a class="nav-link <?php if(PAGE_TITLE == 'Store') echo 'active'; ?>" href="">Store</a>
       </li>
     </ul>
     <ul class="navbar-nav">
       <?php if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true): ?>
         <li class="nav-item">
-          <a href="<?php path('/profile/'); ?>"><span class="navbar-text"><span class="fas fa-user"></span> <?php echo $_SESSION['username']; ?></span></a>
+          <a href=""><span class="navbar-text"><span class="fas fa-user"></span> <?php echo $_SESSION['username']; ?></span></a>
         </li>
         <li class="nav-item d-flex align-items-center">
-          <a href="<?php path('/cart/'); ?>" class="fa-layers fa-fw nav-link">
+          <a href="" class="fa-layers fa-fw nav-link">
             <span class="fas fa-shopping-cart"></span>
             <?php if(isset($_SESSION['cart']) && count($_SESSION['cart']) > 0): ?>
               <span class="fa-layers-counter" style="background:Tomato"></span>
@@ -29,13 +29,13 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php path('/favorites/'); ?>"><span title="View your favorites" class="fas fa-heart"></span></a>
+          <a class="nav-link" href=""><span title="View your favorites" class="fas fa-heart"></span></a>
         </li>
         
       <?php else: ?>
         <div class="ml-auto">
-          <a class="btn btn-secondary navbar-btn" href="<?php path('/login/'); ?>">Sign In</a>
-          <a class="btn btn-success navbar-btn" href="<?php path('/register/'); ?>">Register</a>
+          <a class="btn btn-secondary navbar-btn" href="/login/">Sign In</a>
+          <a class="btn btn-success navbar-btn" href="">Register</a>
         </div>
       <?php endif; ?>
     </ul>
